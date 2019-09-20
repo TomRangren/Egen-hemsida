53fe7ca7467e7363dcd3944208b00d600037d4f0
TomRangren <54435262+TomRangren@users.noreply.github.com>
2019-09-20 20:05:20 +0200

    Update Car History.cs

--- Car History.cs
+++ Car History.cs
@@ -16,8 +16,8 @@
 				</ul>
 			</nav>
 		</header>
-		<main class="box white"><!---->
-			<h2 class="info">First Car</h2> <!---->
+		<main class="box white">
+			<h2 class="info">First Car</h2> 
 				<p> Cars came into global use during the 20th century, and developed economies depend on them. 
 					The year 1886 is regarded as the birth year of the modern car when German inventor Karl Benz patented his Benz Patent-Motorwagen. 
 					Cars became widely available in the early 20th century. One of the first cars accessible to the masses was the 1908 Model T, an American car manufactured by the Ford Motor Company. 
@@ -29,36 +29,36 @@
 					These include rear reversing cameras, air conditioning, navigation systems, and in-car entertainment.
 					Most cars in use in the 2010s are propelled by an internal combustion engine, fueled by the combustion of fossil fuels. 
 					Electric cars, which were invented early in the history of the car, 
-					became commercially available in the 2000s and are predicted to cost less to buy than gasoline cars in the early 2020s.<!---->
+					became commercially available in the 2000s and are predicted to cost less to buy than gasoline cars in the early 2020s.<!--P står för paragraf och om man ska skriva en text på sin sidan så ska man använda p-->
 
-				</p><!---->
-		<table><!---->
-			<Caption>Most Used Cars 2018</Caption><!---->
-				<tr><!---->
-				  <th>Brand</th><!---->
-				  <th>Model</th><!---->
+				</p><!--Avslutar det man skrivit på sin sidan-->
+		<table><!--Öppnar upp möjlighet till en tabell-->
+			<Caption>Most Used Cars 2018</Caption><!--Rubrik till sin tabell-->
+				<tr><!--Skapar en kolumn i tabellen-->
+				  <th>Brand</th><!--Rubrik i kolumn-->
+				  <th>Model</th>
 					
-				</tr><!---->
-				<tr><!---->
-					<td>Toyota</td><!---->
-					<td>F-Series</td><!---->
+				</tr><!--Stänger kolumnen-->
+				<tr>
+					<td>Toyota</td><!--Används som cell data(texten under rubriken i kolumnen-->
+					<td>F-Series</td>
 				   
-				</tr><!---->
-				<tr><!---->
-					<td>Volkswagen</td><!---->
-					<td>Corolla</td>  <!---->
-				</tr><!---->
-				<tr><!---->
-					<td>Ford</td><!---->
-					<td>Civic</td><!---->
-				</tr><!---->
-				<tr><!---->
-					<td>Honda</td><!---->
-					<td>Rav4</td><!---->
-				</tr><!---->
-		</table><!---->
-			<a href="https://www.youtube.com/watch?v=DL_mJeb6O04"> <img src="https://www.daimler.com/bilder/konzern/tradition/geschichte/anfaenge-des-automobils/benz-patent-motorwagen-w520xh208-cutout.png" alt="Picture on first car"> </a><!---->
-			<a class="Link2" href="https://en.wikipedia.org/wiki/Car">Where I got my information</a><!---->
-		</main><!---->
-	</body><!---->
-</html><!---->
+				</tr>
+				<tr>
+					<td>Volkswagen</td>
+					<td>Corolla</td>  
+				</tr>
+				<tr>
+					<td>Ford</td>
+					<td>Civic</td>
+				</tr>
+				<tr>
+					<td>Honda</td>
+					<td>Rav4</td>
+				</tr>
+		</table><!--Avslutar tabellen-->
+			<a href="https://www.youtube.com/watch?v=DL_mJeb6O04"> <img src="https://www.daimler.com/bilder/konzern/tradition/geschichte/anfaenge-des-automobils/benz-patent-motorwagen-w520xh208-cutout.png" alt="Picture on first car"> </a><!--Lägger in en bild som tar användaren till en annan hemsida när man trycker på bilden-->
+			<a class="Link2" href="https://en.wikipedia.org/wiki/Car">Where I got my information</a><!--Sätter in en länk på hemsidan-->
+		</main>
+	</body>
+</html>
